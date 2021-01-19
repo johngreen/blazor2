3c29f1ff9999d2df4b8131466df50e3a15efb735
John <millenniumtoday@gmail.com>
2021-01-19 09:35:08 -0500

    blazor app - basic - has command line blackjack

---
+++ BlazorApp2/BlazorApp2/wwwroot/js/interop.js
@@ -0,0 +1,13 @@
+﻿createAlert = () => {
+	alert("this is an alert.");
+}
+
+createPrompt = (question) => {
+
+	
+	return prompt(question);
+}
+
+setElementTextById = (id, text) => {
+	document.getElementById(id).innerText = text;
+}
